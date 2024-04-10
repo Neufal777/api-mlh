051dfcdd41d48a2f34b03367eed6a8f67ebb572a
Naoufal D <neufal777@gmail.com>
2024-04-10 22:49:15 +0200

    flight engine

--- main.py
+++ main.py
@@ -1,103 +1,36 @@
-from fastapi import FastAPI, HTTPException, Security, Depends
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pydantic import BaseModel
-import uuid
-import time
 
-app = FastAPI()
+import requests
+import json
 
-class PerformanceMiddleware:
-    def __init__(self, app):
-        self.app = app
+class FlightEngine:
+    def __init__(self) -> None:
+        self.base_url = "https://test.api.amadeus.com"
+        self.api_key = ""
+        self.api_secret = ""
 
-    async def __call__(self, scope, receive, send):
-        start_time = time.time()
-        await self.app(scope, receive, send)
-        end_time = time.time()
-        print(f"Request processed in {end_time - start_time} seconds")
 
-app.add_middleware(PerformanceMiddleware)
-
-users = [
-    {"id": str(uuid.uuid4()),"username": "Pranav", "email": "Pranav@ghw.com", "password":"pass", "role":"admin"},
-    {"id": str(uuid.uuid4()),"username": "Kosu", "email": "Kosu@ghw.com", "password":"pass", "role":"user"},
-]
-
-#Pydantic model
-class UserCreate(BaseModel):
-    username: str
-    email: str
-    password: str
-
-class UserResponse(BaseModel):
-    id: str
-    username: str
-    email: str
-
-class UserUpdate(BaseModel):
-    username: str
-    email: str
-
-security = HTTPBasic()
-
-# Check if user is authenticated
-def authenticate(credentials: HTTPBasicCredentials = Security(security)):
-    user = None
-    for u in users:
-        if u["username"] == credentials.username and u["password"] == credentials.password:
-            user = u
-            return user
-    if user is None:
-        raise HTTPException(status_code=401, detail="User is not authenticated must be registered first")
-    return None
-
-# authorize user
-def authorize(user: dict = Depends(authenticate)):
-    if user["role"] != "admin":
-        raise HTTPException(status_code=403, detail="Permission denied")
-    return None
-
-# Get all users if the user is authenticated and haave admin role
-@app.get("/users", response_model=list[UserResponse])
-def get_users(user: dict = Depends(authorize)):
-    return users
-
-# Get user by id if the user is authenticated and haave admin roles
-@app.get("/users/{user_id}", response_model=UserResponse)
-def get_user(user_id: str, user: dict = Depends(authorize)):
-    for u in users:
-        if u["id"] == user_id:
-            return u
-    return {"message": "User not found"}
-
-@app.post("/users")
-def create_user(user: UserResponse):
-    new_user = {
-        "id": str(uuid.uuid4()),
-        "username": user.username,
-        "email": user.email
+    @property
+    def get_token(self):
+        data = {'grant_type': 'client_credentials','client_id': self.api_key, 'client_secret': self.api_secret}
+        
+        response_json = requests.post(f'{self.base_url}/v1/security/oauth2/token', data=data).json()
+        
+        return response_json.get('access_token')
     
-    }
-    users.append(new_user)
-    return new_user, 201
+    def get_flights(self, origin, destination, departure_date, adults, non_stop, max_results):
+        token = self.get_token
+        
+        url = f"{self.base_url}/v2/shopping/flight-offers?originLocationCode={origin}&destinationLocationCode={destination}&departureDate={departure_date}&adults={adults}&nonStop={non_stop}&max={max_results}"
 
-@app.put("/users/{user_id}")
-def update_user(user_id: str, user: UserResponse):
-    for u in users:
-        if u["id"] == user_id:
-            u["username"] = user.username
-            u["email"] = user.email
-            return u
-    return {"message": "User not found"}
+        headers = {'Authorization': f'Bearer {token}'}
 
-@app.delete("/users/{user_id}")
-def delete_user(user_id: str):
-    for i, user in enumerate(users):
-        if user["id"] == user_id:
-            users.pop(i)
-            return {"message": "User deleted successfully"}
-    return {"message": "User not found"}
+        flight_json = requests.request("GET", url, headers=headers).json()
+        
+        for flight in flight_json['data']:
+            print(f"Price: {flight['price']['total']} {flight['price']['currency']}")
+            for itinerary in flight['itineraries']:
+                for segment in itinerary['segments']:
+                    print(f"{segment['departure']['iataCode']} -> {segment['arrival']['iataCode']} ({segment['carrierCode']}{segment['number']} - {segment['duration']} )")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost" , port=8000)
+flights = FlightEngine()
+flights.get_flights("BCN", "SFO", "2024-06-20", 1, "false", 5)
