96cd1c767f9b123e5c26ed7b8c1c3b3490e9de32
Naoufal D <neufal777@gmail.com>
2024-05-11 20:48:08 +0200

    saving changes

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 
 import requests
 import json
+import pandas as pd
 
-class FlightEngine:
-    def __init__(self) -> None:
-        self.base_url = "https://test.api.amadeus.com"
-        self.api_key = ""
-        self.api_secret = ""
+data = pd.read_csv('IndianHealthyRecipe.csv')
 
-
-    @property
-    def get_token(self):
-        data = {'grant_type': 'client_credentials','client_id': self.api_key, 'client_secret': self.api_secret}
+for i in range(len(data['Dish Name'])):
+    dish = data['Dish Name'][i]
+    spice = data['Spice'][i]
+    if spice == 'medium' and 'VEGETARIAN' in data['Dietary Info'][i]:
+        # Prep 10 mins
+        # to thiis: 10 (int)
+        prep_time = data['Prep Time'][i].replace('mins','').replace('Prep','').strip()
         
-        response_json = requests.post(f'{self.base_url}/v1/security/oauth2/token', data=data).json()
-        
-        return response_json.get('access_token')
-    
-    def get_flights(self, origin, destination, departure_date, adults, non_stop, max_results):
-        token = self.get_token
-        
-        url = f"{self.base_url}/v2/shopping/flight-offers?originLocationCode={origin}&destinationLocationCode={destination}&departureDate={departure_date}&adults={adults}&nonStop={non_stop}&max={max_results}"
-
-        headers = {'Authorization': f'Bearer {token}'}
-
-        flight_json = requests.request("GET", url, headers=headers).json()
-        
-        for flight in flight_json['data']:
-            print(f"Price: {flight['price']['total']} {flight['price']['currency']}")
-            for itinerary in flight['itineraries']:
-                for segment in itinerary['segments']:
-                    print(f"{segment['departure']['iataCode']} -> {segment['arrival']['iataCode']} ({segment['carrierCode']}{segment['number']} - {segment['duration']} )")
-
-flights = FlightEngine()
-flights.get_flights("BCN", "SFO", "2024-06-20", 1, "false", 5)
+        # convert to integer
+        prep_time = int(prep_time)
+        print(f"[🕒 {prep_time}] 🍔 {dish} {spice.replace('medium','🌶️')} {data['Dietary Info'][i]}")
