7f8b11c7db9ceb3bdc152a3c2047d3fc6a304937
Naoufal D <neufal777@gmail.com>
2024-05-13 21:51:14 +0200

    scrapping mlh website

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-
 import requests
+from bs4 import BeautifulSoup
 import json
-import pandas as pd
+headers = {
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0',
+ 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+ 'Accept-Language': 'en-US,en;q=0.5',
+ 'Referer': 'https://mlh.io/',
+ 'DNT': '1',
+ 'Connection': 'keep-alive',
+ 'Upgrade-Insecure-Requests': '1',
+}
 
-data = pd.read_csv('IndianHealthyRecipe.csv')
+response = requests.get('https://mlh.io/seasons/2024/events', headers=headers)
 
-for i in range(len(data['Dish Name'])):
-    dish = data['Dish Name'][i]
-    spice = data['Spice'][i]
-    if spice == 'medium' and 'VEGETARIAN' in data['Dietary Info'][i]:
-        # Prep 10 mins
-        # to thiis: 10 (int)
-        prep_time = data['Prep Time'][i].replace('mins','').replace('Prep','').strip()
-        
-        # convert to integer
-        prep_time = int(prep_time)
-        print(f"[🕒 {prep_time}] 🍔 {dish} {spice.replace('medium','🌶️')} {data['Dietary Info'][i]}")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+events = soup.find_all('div', class_='event-wrapper')
+events_mapping = []
+
+for event in events:
+    name = event.find('h3', class_='event-name').text
+    start_date = event.find('meta', itemprop='startDate')['content']
+    end_date = event.find('meta', itemprop='endDate')['content']
+    location = event.find('div', class_='event-location').text.strip()
+    location = location.replace('\n', '')
+    link = event.find('a')['href']
+
+    events_mapping.append({
+        'name': name,
+        'link': link,
+        'start_date': start_date,
+        'end_date': end_date,
+        'location': location
+    })
+
+
+for event in events_mapping:
+    print("Event Name: ", event['name'])
+    print("Start Date: ", event['start_date'])
+    print("End Date: ", event['end_date'])
+    print("Link: ", event['link'])
+    print("Location: ", event['location'])
+    print("=====================================")
+    
+
+    
